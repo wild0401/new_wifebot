71b1990982e671df9c96cc1597104630f1bb8113
wild0401 <k7070707@gmail.com>
2020-12-11 00:34:22 +0900

    1211

--- cmds/event.py
+++ cmds/event.py
@@ -42,7 +42,41 @@ class Event(Cog_Extension):
         else:
             await ctx.send("發生錯誤")
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        #新增反應貼圖獲取身分組
+        #判斷反應訊息是否為指定的訊息
+        if payload.message_id == 786301223627784202:
+            #判斷反應貼圖給予相對應身分組
+            if str(payload.emoji) == '💯':
+                #print("有近來")
+                guild = self.bot.get_guild(payload.guild_id)    #取得當前所在伺服器
+                role = guild.get_role(786288390827016222)       #取得伺服器內指定的身分組                     
+                await payload.member.add_roles(role)            #給予該成員身分組
+                await payload.member.send(f"你取得了{role}身分組!")
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        #新增反應貼圖獲取身分組
+        if payload.message_id == 786301223627784202:
+            #判斷反應貼圖給予相對應身分組
+            if str(payload.emoji) == '💯':
+                #print("有近來")
+                guild = self.bot.get_guild(payload.guild_id)    #取得當前所在伺服器
+                user = guild.get_member(payload.user_id)
+                role = guild.get_role(786288390827016222)        #取得伺服器內指定的身分組                     
+                await user.remove_roles(role)                    #移除該成員身分組
+                await user.send(f"你移除了{role}身分組!")
+    
+    @commands.Cog.listener()
+    async def on_message_delete(self, msg):
+        counter = 1
+        async for audilog in msg.guild.audit_logs(action=discord.AuditLogAction.message_delete):
+            if counter == 1:
+                await msg.channel.send(audilog.user.name)
+                counter += 1
+        #await msg.channel.send("刪除訊息內容 : " + str(msg.content))
+        #await msg.channel.send("刪除訊息原本的作者 : " + str(msg.author))
 
 def setup(bot):
     bot.add_cog(Event(bot))
